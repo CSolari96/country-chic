4079512cf5b01bb6846531c9256fcd26e7470e6c
Courtney Solari <csolari@ufl.edu>
2020-02-24 15:55:36 -0600

    Set up header.php

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=content-device-width, initial-scale=1.0">
+
+	<title><?php bloginfo('name')l ?></title>
+
+	<?php wp_head(); ?>
+
+</head>
+
+<body>
+
+	<header>
+
+		<nav>
+
+		</nav>
+
+	</header>

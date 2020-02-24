7b0fbae521921324b52a2c8b1d9356f6451ec9f6
Courtney Solari <csolari@ufl.edu>
2020-02-24 15:52:55 -0600

    Set up functions.php file

---
+++ functions.php
@@ -0,0 +1,35 @@
+<?php
+
+	/**********Add stylesheet and JS files**********/
+	function custom_theme_scripts() {
+
+		// Main CSS
+		wp_enqueue_style('main-styles', get_stylesheet_uri());
+
+		// Javascript Files
+		wp_enqueue_script('custom-js', get_template_directory_uri() . '/js/main.js');
+	}
+
+	add_action('wp_enqueue_scripts', 'custom_theme_scripts');
+
+	/**********Custom Header Logo**********/
+
+
+	/**********Add menus to theme**********/
+	function register_my_menu() {
+		register_nav_menus(
+			array(
+				'top-menu' => ('Top Menu')
+			)
+		);
+	}
+
+	add_action('init', 'register_my_menu');
+
+	/**********Add Widgets**********/
+
+
+	/**********Add Featured Image Capability**********/
+	add_theme_support('post-thumbnails');
+
+?>

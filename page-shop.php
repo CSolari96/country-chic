c94788b4343f92ab5bb6ce3624a6180350e1384f
Courtney Solari <csolari@ufl.edu>
2020-03-11 11:24:11 -0500

    Test shopping cart function

---
+++ page-shop.php
@@ -0,0 +1,13 @@
+<?php 
+	
+	get_header(); 
+
+	$Cart_Icon = WP_Shopify\Factories\Render\Cart\Cart_Factory::build();
+
+	$Cart->cart_icon(['icon_color'=>"#63a497"]);
+?>
+
+
+
+
+<?php get_footer(); ?>

1e06205ab83c341abea23afc8532c33591434cfa
Courtney Solari <csolari@ufl.edu>
2020-03-11 11:35:31 -0500

    Test cart icon

--- page-shop.php
+++ page-shop.php
@@ -11,7 +11,11 @@
 
 	$Cart_Icon = WP_Shopify\Factories\Render\Cart\Cart_Factory::build();
 
-	echo $Cart->cart_icon(['icon_color'=>"#63a497"]);
+	// Show cart icon with custom image
+	$Cart->cart_icon([
+	   'icon' => "https://freeiconshop.com/wp-content/uploads/edd/cart-outline.png"
+	]);
+
 ?>
 
 

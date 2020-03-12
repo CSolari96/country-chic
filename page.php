61ec65129f5a11ec035982fa23f8d5e76d5f516c
Courtney Solari <csolari@ufl.edu>
2020-03-11 19:37:31 -0500

    Fix error in page.php

--- page.php
+++ page.php
@@ -5,20 +5,18 @@
 	<div class="row">
 
 		<?php
+					if (have_posts()) {
+						while (have_posts()) {
+							the_post(); ?>
 
-			if (have_posts()) {
-				while (have_posts()) {
-					the_post(); ?>
+							<h2><?php the_title(); ?></h2>
 
-					<h2><?php the_title(); ?></h2>
+							<?php the_content(); ?>
+				<?php
+						}  // End while
+					}  // End if
 
-					<?php the_content(); ?>
-			<?php
-
-				}
-			}
-
-		?>
+				?>
 
 	</div>
 

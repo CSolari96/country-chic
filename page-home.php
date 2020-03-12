c64436b312b991c80de8bc628ffe6410440e18a8
Courtney Solari <csolari@ufl.edu>
2020-03-11 20:55:40 -0500

    Create home page template file

---
+++ page-home.php
@@ -0,0 +1,37 @@
+<?php 
+
+	/* 
+
+	Template Name: Home Page Layout
+	Template Post Type: page
+
+	*/
+
+	get_header(); 
+
+?>
+
+<main class="container">
+
+	<div class="row">
+
+		<?php
+					if (have_posts()) {
+						while (have_posts()) {
+							the_post(); ?>
+
+							<h2><?php the_title(); ?></h2>
+
+							<?php the_content(); ?>
+				<?php
+						}  // End while
+					}  // End if
+
+				?>
+
+	</div>
+
+</main>
+
+
+<?php get_footer(); ?>

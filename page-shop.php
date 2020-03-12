22942c7d161d37e26abfd006dd21786e0ee77af0
Courtney Solari <csolari@ufl.edu>
2020-03-11 21:22:41 -0500

    Adjust shop page template content

--- page-shop.php
+++ page-shop.php
@@ -6,10 +6,18 @@
 		Template Post Type: page
 
 	*/
-	
+
 	get_header();
 
 	$Products = WP_Shopify\Factories\Render\Products\Products_Factory::build();
+	$Settings = WP_Shopify\Factories\DB\Settings_General_Factory::build();
+	   
+	$products_options = [
+      	'excludes' => ['description'],
+		'excludes' => ['buy-button']
+  	];
+
+	$args = apply_filters('wps_products_all_args', $products_options);
 
 	?>
 
@@ -27,14 +35,10 @@
 			   <?= do_action('wps_breadcrumbs') ?>
 
 			   <div class="wps-products-all">
+			      
+			      <?php
 
-			      <?php 
-
-				      global $post;
-
-				      $Products->products(['title' => $post->post_title]); 
-
-				      $Products->pricing(['title' => $post->post_title]);
+			      	$Products->products($args); 
 			      
 			      ?>
 
@@ -45,5 +49,6 @@
 		</div>
 
 	</main>
+
 
 <?php get_footer(); ?>

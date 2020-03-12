90d2353322b085e9b85f2902563fa2f99e22908d
Courtney Solari <csolari@ufl.edu>
2020-03-11 19:15:54 -0500

    Set up default page template

--- page.php
+++ page.php
@@ -1,5 +1,28 @@
 <?php get_header(); ?>
 
+<main class="container">
+
+	<div class="row">
+
+		<?php
+
+			if (have_posts()) {
+				while (have_posts)) {
+					the_post(); ?>
+
+					<h2><?php the_title(); ?></h2>
+
+					<?php the_content(); ?>
+			<?php
+
+				}
+			}
+
+		?>
+
+	</div>
+
+</main>
 
 
 <?php get_footer(); ?>

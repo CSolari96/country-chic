e31424264bd150c229a08019dad7a0905d2b5fbc
Valerie Bragg <vbragg@ufledu>
2020-03-12 19:40:00 -0400

    Fix critical error

--- page-contact.php
+++ page-contact.php
@@ -14,11 +14,11 @@
     <div class="row">
       <main class="col-md-12">
         <?php
-          if(have_posts()){
+          if(have_posts())  {
             while(have_posts()){
               the_post(); ?>
 
-              <h2><?php the_title(); ?></h2>
+              <?php the_title(); ?>
 
               <?php the_content(); ?>
 

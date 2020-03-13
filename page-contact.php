7e85e9a7f40144f89d11b8e3f58660ba61761b8b
Valerie Bragg <vbragg@ufledu>
2020-03-12 20:44:02 -0400

    Add contact page

---
+++ page-contact.php
@@ -0,0 +1,38 @@
+<?php
+
+	/*
+
+	Template Name: Contact About Layout
+	Template Post Type: page
+
+	*/
+
+?>
+
+
+<?php get_header(); ?>
+
+<main class="container-fluid">
+
+	<div class="row">
+
+		<?php
+					if (have_posts()) {
+						while (have_posts()) {
+							the_post(); ?>
+
+							<h2><?php the_title(); ?></h2>
+
+							<?php the_content(); ?>
+				<?php
+						}  // End while
+					}  // End if
+
+				?>
+
+	</div>
+
+</main>
+
+
+<?php get_footer(); ?>

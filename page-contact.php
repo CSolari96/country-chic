e4fba04a2c4afa20a5a8d46defc3e91e854c13f3
Valerie Bragg <vbragg@ufledu>
2020-03-13 19:45:59 -0400

    Break contact page into sections

--- page-contact.php
+++ page-contact.php
@@ -21,11 +21,15 @@
 
 		</div>
 
-		<div class="row">
+		<section>
+			<div class="row">
 				<div class="col-12">
 					<?php dynamic_sidebar('contact-page-text'); ?>
 				</div>
+			</div>
+			</section>
 
+		<section>
 				<div class="row">
 						<div class="col-6">
 							<?php dynamic_sidebar('contact-page-phone-number'); ?>
@@ -33,7 +37,6 @@
 						<div class="col-6">
 							<img src="../images/phone.png" alt="Telephone icon"> </img>
 						</div>
-
 					</div>
 
 					<div class="row">
@@ -43,25 +46,24 @@
 							<div class="col-6">
 								<img src="../images/paper-plane.png" alt="Mail icon"> </img>
 							</div>
-
 						</div>
+			</section>
 
-			</div>
-
-			<div class="row">
-					<div class="col-12">
-							<?php dynamic_sidebar('contact-form'); ?>
-					</div>
-
+			<section>
+				<div class="row">
+						<div class="col-12">
+								<?php dynamic_sidebar('contact-form'); ?>
+						</div>
 				</div>
+			</section>
 
+			<section>
 				<div class="row">
 						<div class="col-12">
 							<?php dynamic_sidebar('contact-page-hours'); ?>
-
 						</div>
-
 					</div>
+			</section>
 
 	</main>
 

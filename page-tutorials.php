8ade0aeabb21e8df749363386f5fc3688739ee1d
Valerie Bragg <vbragg@ufledu>
2020-03-15 22:20:39 -0400

    Add jumbotron and content to tutorials page

--- page-tutorials.php
+++ page-tutorials.php
@@ -11,7 +11,36 @@
 
 ?>
 
+<div class="jumbotron jumbotron-fluid" style="background-image: url(<?php the_post_thumbnail('full'); ?>)">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1> <?php the_title(); ?> </h1>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="post-featured-image">
+	<?php the_post_thumbnail('large'); ?>
+</div>
+
 <main class="container-fluid">
+
+	<section>
+			<div class="row col-12">
+				<?php
+					if(have_posts()){
+						while(have_posts()){
+							the_post(); ?>
+
+							<?php the_content(); ?>
+					<?php  }//ends while loop
+					}//ends the if statement
+				 ?>
+				</div>
+		</section>
 
 	<div class="row">
 
@@ -31,9 +60,9 @@
 		</div>
 
 			<div class="col-md-3">
-				<h3>Country Chic Products Used</h3>
-				<?php the_field('product'); ?>
-				<?php the_field('brushes_used'); ?>
+				<h4>Country Chic Products Used</h4>
+				<p class="products-used"><?php the_field('product'); ?></p>
+				</p class="products-used"><?php the_field('brushes_used'); ?></p>
 			</div>
 
 	<?php	} ?>

650648654d0d048616869360b989da4dd10ed6fc
Valerie Bragg <vbragg@ufledu>
2020-03-20 15:47:14 -0400

    Add code to pop up on values section in about

--- page-about.php
+++ page-about.php
@@ -121,28 +121,28 @@
 
 				</div>
 
-				<div class="row center col-sm-12 col-md-6 col-lg-3">
+				<div class="row center pop-up-parent col-sm-12 col-md-6 col-lg-3">
 
 					<?php dynamic_sidebar('low-impact-value-icon'); ?>
 					<?php dynamic_sidebar('low-impact-value'); ?>
 
 				</div>
 
-				<div class="row center col-sm-12 col-md-6 col-lg-3">
+				<div class="row center pop-up-parent col-sm-12 col-md-6 col-lg-3">
 
 					<?php dynamic_sidebar('recycling-programs-value-icon'); ?>
 					<?php dynamic_sidebar('recycling-programs-value'); ?>
 
 				</div>
 
-				<div class="row center col-sm-12 col-md-6 col-lg-3">
+				<div class="row center pop-up-parent col-sm-12 col-md-6 col-lg-3">
 
 					<?php dynamic_sidebar('local-economy-value-icon'); ?>
 					<?php dynamic_sidebar('local-economy-value'); ?>
 
 				</div>
 
-				<div class="row center col-sm-12 col-md-6 col-lg-3">
+				<div class="row center pop-up-parent col-sm-12 col-md-6 col-lg-3">
 
 					<?php dynamic_sidebar('those-in-need-value-icon'); ?>
 					<?php dynamic_sidebar('those-in-need-value'); ?>

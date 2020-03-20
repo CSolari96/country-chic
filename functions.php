ebde0aed3261aac113f776882cb8e881677dafd3
Valerie Bragg <vbragg@ufledu>
2020-03-20 16:49:31 -0400

    Fix h4s in values sections

--- functions.php
+++ functions.php
@@ -535,8 +535,8 @@ register_sidebar(array(
 			'description' => 'Participating in Recylcling Programs value icon',
 			'before_widget' => '<div class="about-values-icon">',
 			'after_widget' => '</div>',
-			'before_title' => '<h3 class="hide">',
-			'after_title' => '</h3>'
+			'before_title' => '<h4 class="values">',
+			'after_title' => '</h4>'
 			));
 
 			register_sidebar(array(
@@ -545,8 +545,8 @@ register_sidebar(array(
 				'description' => 'Helping those in need value icon',
 				'before_widget' => '<div class="about-values-icon">',
 				'after_widget' => '</div>',
-				'before_title' => '<h3 class="hide">',
-				'after_title' => '</h3>'
+				'before_title' => '<h4 class="values">',
+				'after_title' => '</h4>'
 				));
 
 

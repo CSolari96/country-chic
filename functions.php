4e9b23fd524f94d2fb3f671748e1cc0ff94b0430
Valerie Bragg <vbragg@ufledu>
2020-03-19 20:09:23 -0400

    Fix paragraph spacing issue on about page

--- functions.php
+++ functions.php
@@ -444,8 +444,8 @@ register_sidebar(array(
 	'description' => 'A video describing Country Chic',
 	'before_widget' => '<div class="video-container">',
 	'after_widget' => '</div>',
-	'before_title' => '<h3>',
-	'after_title' => '</h3>'
+	'before_title' => '<h2>',
+	'after_title' => '</h2>'
 ));
 
 	register_sidebar(array(

20ef83c4ccdf15ce517198d6e4b76462bbe733ad
Valerie Bragg <vbragg@ufledu>
2020-03-20 21:16:10 -0400

    Take out img link to correct error

--- page-tutorials.php
+++ page-tutorials.php
@@ -50,14 +50,12 @@
 		while($loop->have_posts()): $loop->the_post();?>
 
 			<div class="card" style="width: 18rem;">
-  			<img src="<?php if(has_post_thumbnail()) {
-				the_post_thumbnail('medium'); ?>" >
+  			<img src=""> 
   			<div class="card-body">
     			<h5 class="card-title">Card title</h5>
     			<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     			<a href="#" class="btn btn-primary">Go somewhere</a>
   			</div>
-				<?php	} ?>
 			</div>
 
 			<?php endwhile; ?>

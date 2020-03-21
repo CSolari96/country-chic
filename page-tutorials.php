1b6531a96695880f1654a38174e3b0541437ab0e
Valerie Bragg <vbragg@ufledu>
2020-03-20 21:28:33 -0400

    Change tags around image on cards

--- page-tutorials.php
+++ page-tutorials.php
@@ -49,8 +49,8 @@
 
 		while($loop->have_posts()): $loop->the_post();?>
 
-			<div class="card" style="width: 18rem;">
-  			<img class="card-picture"><?php the_post_thumbnail('small'); ?> </img> 
+			<div class="card tutorial-cards" style="width: 18rem;">
+  			<?php the_post_thumbnail('small'); ?>
   			<div class="card-body">
     			<h5 class="card-title">Card title</h5>
     			<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>

e1f9cc6ae8a4bf56eb8b041c6672da92efe3ba83
Courtney Solari <csolari@ufl.edu>
2020-03-22 16:24:34 -0500

    Make all headers consistent

--- page-tutorials.php
+++ page-tutorials.php
@@ -15,7 +15,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<h1> <?php the_title(); ?> </h1>
+				<h1 class="hero-title"> <?php the_title(); ?> </h1>
 			</div>
 		</div>
 	</div>

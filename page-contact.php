8c9f5a3054ea738b9f2044c54e29a63875548da1
Valerie Bragg <vbragg@ufledu>
2020-03-22 18:09:59 -0400

    Delete random symbols appearing in top left on contact page

--- page-contact.php
+++ page-contact.php
@@ -12,7 +12,7 @@
 
 <?php get_header(); ?>
 
-	<div class="jumbotron jumbotron-fluid" style="background-image: url(<?php the_post_thumbnail('full'); ?>)">
+	<div class="jumbotron jumbotron-fluid" style="background-image: url(<?php echo $thumb_url; ?>)">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">

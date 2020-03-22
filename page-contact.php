fcd89b95e201002f6881fe09ef14105bfb04532e
Valerie Bragg <vbragg@ufledu>
2020-03-22 17:18:35 -0400

    Delete text that was appearing in top corner of contact page

--- page-contact.php
+++ page-contact.php
@@ -54,7 +54,7 @@
 						<?php dynamic_sidebar('contact-page-email'); ?>
 				</div>
 
-				<div class="row values-container contact-form-section" style="background-image: url('<?php echo get_template_directory_uri() . "/images/values_bkg.png"; ?>')">
+				<div class="row values-container contact-form-section" style="background-image: url('<?php echo get_template_directory_uri() . '/images/values_bkg.png'; ?>')">
 						<?php dynamic_sidebar('contact-form'); ?>
 				</div>
 

3481a232d01c921814ef45354b84ac57949d2e9a
Valerie Bragg <vbragg@ufledu>
2020-04-02 11:00:14 -0400

    Add reset postdata

--- page-tutorials.php
+++ page-tutorials.php
@@ -67,22 +67,9 @@
 				  			</div>
 						</div>
 
-						<div class="pagination-links change"> 
-							<?php
-								 global $wp_query;
-
-								 $big = 999999999; // need an unlikely integer
-
-								 echo paginate_links( array(
-								 'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-								 'format' => '?paged=%#%',
-								 'current' => max( 1, get_query_var('paged') ),
-								 'total' => $wp_query->max_num_pages
-									) );
-							?>
-						</div>
-
 					<?php endwhile; ?>
+					
+					<?php wp_reset_postdata(); ?>
 
 
 		</section>

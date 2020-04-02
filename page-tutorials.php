d60766ce3a245ff1f59647398fa5b5234eeb0165
Valerie Bragg <vbragg@ufledu>
2020-04-01 23:08:20 -0400

    Add the posts pagination

--- page-tutorials.php
+++ page-tutorials.php
@@ -44,19 +44,7 @@
 						<?php  }//ends while loop ?>
 
 						<div class ="pagination-links">
-							<?php
-					       global $wp_query;
-
-					       $big = 999999999; // need an unlikely integer
-
-					       echo paginate_links( array(
-					       'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-					       'format' => '?paged=%#%',
-					       'current' => max( 1, get_query_var('paged') ),
-					       'total' => $wp_query->max_num_pages
-					        ) );
-
-									?>
+							<?php the_posts_pagination();?>
 						</div>
 						<?php }//ends the if statement
 					 ?>

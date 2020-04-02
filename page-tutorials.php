d4471d63f7b06f3fad36f5f91c01498523046581
Valerie Bragg <vbragg@ufledu>
2020-04-02 18:47:20 -0400

    Alter variables  for pagination to see if that makes links work

--- page-tutorials.php
+++ page-tutorials.php
@@ -71,18 +71,18 @@
 
 				<div class="row col-12 pagination">
 					<?php
-					$wp_query = new WP_Query($args);
+					$wpex_query = new WP_Query($args);
 
-					global $wp_query;
+					global $wp_query, $wpex_query;
 
-       $big = 999999999; // need an unlikely integer
+			       $big = 999999999; // need an unlikely integer
 
-       echo paginate_links( array(
-       'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-       'format' => '?paged=%#%',
-       'current' => max( 1, get_query_var('paged') ),
-       'total' => $wp_query->max_num_pages
-        ) );
+			       echo paginate_links( array(
+			       'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+			       'format' => '?paged=%#%',
+			       'current' => max( 1, get_query_var('paged') ),
+			       'total' => $wp_query->max_num_pages
+			        ) );
 
 ?>
 </div>

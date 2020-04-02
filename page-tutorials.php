c52ebc6eb215edd78fe9076ac2697a34924a15e5
Valerie Bragg <vbragg@ufledu>
2020-04-02 11:25:16 -0400

    Pagnization change 7

--- page-tutorials.php
+++ page-tutorials.php
@@ -68,8 +68,20 @@
 						</div>
 
 					<?php endwhile; ?>
-					
-					<?php wp_reset_postdata(); ?>
+
+					<?php
+       global $wp_query;
+
+       $big = 999999999; // need an unlikely integer
+
+       echo paginate_links( array(
+       'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+       'format' => '?paged=%#%',
+       'current' => max( 1, get_query_var('paged') ),
+       'total' => $wp_query->max_num_pages
+        ) );
+
+?>
 
 
 		</section>

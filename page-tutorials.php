55c56d159998bcb51de5342b8a5f472db40a1ba9
Valerie Bragg <vbragg@ufledu>
2020-04-01 22:58:20 -0400

    Add page numbers to main tutorials page

--- page-tutorials.php
+++ page-tutorials.php
@@ -41,8 +41,24 @@
 								the_post(); ?>
 
 								<?php the_content(); ?>
-						<?php  }//ends while loop
-						}//ends the if statement
+						<?php  }//ends while loop ?>
+
+						<div class ="pagination-links">
+							<?php
+					       global $wp_query;
+
+					       $big = 999999999; // need an unlikely integer
+
+					       echo paginate_links( array(
+					       'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+					       'format' => '?paged=%#%',
+					       'current' => max( 1, get_query_var('paged') ),
+					       'total' => $wp_query->max_num_pages
+					        ) );
+
+									?>
+						</div>
+						<?php }//ends the if statement
 					 ?>
 				 </div>
 				</div>

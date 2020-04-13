b3f0c4b7e937cdf2bb9677537a163dec58542414
Courtney Solari <csolari@ufl.edu>
2020-04-13 16:36:46 -0500

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+<main class="container-fluid">
+
+	<div class="row">
+
+		<div class="col-4">
+
+			<img src="<?php echo get_template_directory_uri() . '/images/CCP_Lustrum_BrushPile3.jpg'?>" alt="Pile of Paint Brushes">
+
+		</div>
+
+		<div class="col-4">
+
+			<?php dynamic_sidebar("404-content"); ?>
+
+		</div>
+	</div>
+
+</main>
+
+
+<?php get_footer(); ?>

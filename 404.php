3440bdf9d2f94ec203b058a37951d6cc986f3fa1
Courtney Solari <csolari@ufl.edu>
2020-04-13 17:14:36 -0500

    Edit 404 page

--- 404.php
+++ 404.php
@@ -2,15 +2,15 @@
 
 <main class="container-fluid">
 
-	<div class="row 404-page">
+	<div class="row page-404">
 
-		<div class="col-4">
+		<div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
 
 			<img src="<?php echo get_template_directory_uri() . '/images/brushPile.png'?>" alt="Pile of Paint Brushes">
 
 		</div>
 
-		<div class="col-4">
+		<div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
 
 			<?php dynamic_sidebar("404-content"); ?>
 

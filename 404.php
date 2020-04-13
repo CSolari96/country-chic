5d94f8961e053f638aa6a5fb64d4ef3fcfd11a60
Courtney Solari <csolari@ufl.edu>
2020-04-13 17:03:03 -0500

    Modify 404 page layout

--- 404.php
+++ 404.php
@@ -4,17 +4,12 @@
 
 	<div class="row">
 
-		<div class="col-4">
-
-			<img src="<?php echo get_template_directory_uri() . '/images/brushPile.png'?>" alt="Pile of Paint Brushes">
-
-		</div>
-
-		<div class="col-4">
+		<div class="col-12">
 
 			<?php dynamic_sidebar("404-content"); ?>
 
 		</div>
+
 	</div>
 
 </main>

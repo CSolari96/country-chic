601879bb31e8f5fcd2e1a92b162a9923468e15aa
Courtney Solari <csolari@ufl.edu>
2020-04-15 15:45:42 -0500

    Load shop.js once everything else is loaded

--- js/shop.js
+++ js/shop.js
@@ -1,29 +1,32 @@
-// Create new link
-var newLink = document.createElement("a");
-var linkText = document.createTextNode("Go to the previous page");
-newLink.appendChild(linkText);
+window.addEventListener("load", function(event) {
+	// Create new link
+	var newLink = document.createElement("a");
+	var linkText = document.createTextNode("Go to the previous page");
+	newLink.appendChild(linkText);
 
-// Set link attributes
-newLink.href = document.referrer;
-newLink.id = "back-link";
+	// Set link attributes
+	newLink.href = document.referrer;
+	newLink.id = "back-link";
 
-// Locate and store product or collection container
-var collectionContainer = document.getElementsByClassName("wps-collection-single")[0];
-var productContainer = document.getElementsByClassName("wps-product-single")[0];
+	// Locate and store product or collection container
+	var collectionContainer = document.getElementsByClassName("wps-collection-single")[0];
+	var productContainer = document.getElementsByClassName("wps-product-single")[0];
 
-// Create variable to hold page container
-var pageContainer = null;
+	// Create variable to hold page container
+	var pageContainer = null;
 
-// Check if it is a single product or single collection page and set appropriate page container
-if (typeof(collectionContainer) != 'undefined' && collectionContainer != null) {
-	pageContainer = collectionContainer.parentElement;
-	addLink();
-} else if (typeof(productContainer) != 'undefined' && productContainer != null) {
-	pageContainer = productContainer.parentElement;
-	addLink();
-}
+	// Check if it is a single product or single collection page and set appropriate page container
+	if (typeof(collectionContainer) != 'undefined' && collectionContainer != null) {
+		pageContainer = collectionContainer.parentElement;
+		addLink(collectionContainer);
+	} else if (typeof(productContainer) != 'undefined' && productContainer != null) {
+		pageContainer = productContainer.parentElement;
+		addLink(productContainer);
+	}
 
-// Add link above product information
-function addLink() {
-	pageContainer.insertBefore(newLink, productContainer);
-}
+	// Add link above product information
+	function addLink(container) {
+		pageContainer.insertBefore(newLink, container);
+	}
+});
+

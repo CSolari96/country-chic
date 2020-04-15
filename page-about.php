c6c83516364e2d004d054354df75aedc902f60ec
Courtney Solari <csolari@ufl.edu>
2020-04-15 16:41:32 -0500

    Adjust background image placement

--- page-about.php
+++ page-about.php
@@ -11,21 +11,17 @@
 
 <?php get_header(); ?>
 
-	<div class="about">
+	<?php $background_img = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full'); ?>
 
-		<?php $background_img = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full'); ?>
+	<div class="hero-widget-content page-header-banner bkg-center" style="background-image: url(<?php echo $background_img[0]; ?>)">
 
-		<div class="hero-widget-content page-header-banner" style="background-image: url(<?php echo $background_img[0]; ?>)">
+		<div class="container-fluid">
 
-			<div class="container-fluid">
+			<div class="row">
 
-				<div class="row">
+				<div class="col-md-12">
 
-					<div class="col-md-12">
-
-						<h1 class="hero-title"> <?php the_title(); ?> </h1>
-
-					</div>
+					<h1 class="hero-title"> <?php the_title(); ?> </h1>
 
 				</div>
 
@@ -33,135 +29,134 @@
 
 		</div>
 
-		<main class="container">
-
-			<section class="line-spacing">
-
-					<div class="row justify-content-center">
-						<div class="col-md-12">
-						<?php dynamic_sidebar('secondary-title-about'); ?>
-						</div>
-					</div>
-
-					<div class="row justify-content-center">
-						<div class="col-md-12">
-						<?php
-				          if(have_posts()){
-				            while(have_posts()){
-				              the_post(); ?>
-
-				              <?php the_content(); ?>
-				          <?php  }//ends while loop
-				          }//ends the if statement
-				         ?>
-					 	</div>
-					</div>
-
-			</section>
-
-			<section>
-
-				<div class="row">
-					<div class="col-12">
-							<?php dynamic_sidebar('video-about-us'); ?>
-					</div>
-				</div>
-
-			</section>
-
-			<section class="row center card-images-about">
-
-				<div class="meet-header col-12">
-					<?php dynamic_sidebar('meet-us-title'); ?>
-				</div>
-
-				<div class="row center col-sm-12 col-md-6 col-xl-3 animated fadeIn eds-on-scroll ">
-					<div class="hover-images-effects">
-					  <div class="card card-third">
-					    <?php dynamic_sidebar('rosanne-picture'); ?>
-					    <div class="card--hidden">
-								<?php dynamic_sidebar('rosanne-picture-info'); ?>
-					    </div>
-					  </div>
-					</div>
-				</div>
-
-				<div class="row center col-sm-12 col-md-6 col-xl-3 animated fadeIn eds-on-scroll">
-					<div class="hover-images-effects">
-						<div class="card card-third">
-							<?php dynamic_sidebar('jan-picture'); ?>
-							<div class="card--hidden">
-								<?php dynamic_sidebar('jan-picture-info'); ?>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="row center col-sm-12 col-md-6 col-xl-3 animated fadeIn eds-on-scroll">
-					<div class="hover-images-effects">
-						<div class="card card-third">
-							<?php dynamic_sidebar('sarah-picture'); ?>
-							<div class="card--hidden">
-								<?php dynamic_sidebar('sarah-picture-info'); ?>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="row center col-sm-12 col-md-6 col-xl-3 animated fadeIn eds-on-scroll">
-					<div class="hover-images-effects">
-						<div class="card card-third">
-							<?php dynamic_sidebar('christa-picture'); ?>
-							<div class="card--hidden">
-								<?php dynamic_sidebar('christa-picture-info'); ?>
-							</div>
-						</div>
-					</div>
-				</div>
-
-			</section>
-
-			<section class="row">
-
-				<div class="col-md-12">
-
-					<?php dynamic_sidebar('about-values-title'); ?>
-
-				</div>
-
-				<div class="row center col-sm-12 col-md-6">
-
-					<?php dynamic_sidebar('low-impact-value-icon'); ?>
-					<?php dynamic_sidebar('low-impact-value'); ?>
-
-				</div>
-
-				<div class="row center col-sm-12 col-md-6">
-
-					<?php dynamic_sidebar('recycling-programs-value-icon'); ?>
-					<?php dynamic_sidebar('recycling-programs-value'); ?>
-
-				</div>
-
-				<div class="row center col-sm-12 col-md-6">
-
-					<?php dynamic_sidebar('local-economy-value-icon'); ?>
-					<?php dynamic_sidebar('local-economy-value'); ?>
-
-				</div>
-
-				<div class="row center col-sm-12 col-md-6">
-
-					<?php dynamic_sidebar('those-in-need-value-icon'); ?>
-					<?php dynamic_sidebar('those-in-need-value'); ?>
-
-				</div>
-
-			</section>
-
-		</main>
-
 	</div>
 
+	<main class="container">
+
+		<section class="line-spacing">
+
+				<div class="row justify-content-center">
+					<div class="col-md-12">
+					<?php dynamic_sidebar('secondary-title-about'); ?>
+					</div>
+				</div>
+
+				<div class="row justify-content-center">
+					<div class="col-md-12">
+					<?php
+			          if(have_posts()){
+			            while(have_posts()){
+			              the_post(); ?>
+
+			              <?php the_content(); ?>
+			          <?php  }//ends while loop
+			          }//ends the if statement
+			         ?>
+				 	</div>
+				</div>
+
+		</section>
+
+		<section>
+
+			<div class="row">
+				<div class="col-12">
+						<?php dynamic_sidebar('video-about-us'); ?>
+				</div>
+			</div>
+
+		</section>
+
+		<section class="row center card-images-about">
+
+			<div class="meet-header col-12">
+				<?php dynamic_sidebar('meet-us-title'); ?>
+			</div>
+
+			<div class="row center col-sm-12 col-md-6 col-xl-3 animated fadeIn eds-on-scroll ">
+				<div class="hover-images-effects">
+				  <div class="card card-third">
+				    <?php dynamic_sidebar('rosanne-picture'); ?>
+				    <div class="card--hidden">
+							<?php dynamic_sidebar('rosanne-picture-info'); ?>
+				    </div>
+				  </div>
+				</div>
+			</div>
+
+			<div class="row center col-sm-12 col-md-6 col-xl-3 animated fadeIn eds-on-scroll">
+				<div class="hover-images-effects">
+					<div class="card card-third">
+						<?php dynamic_sidebar('jan-picture'); ?>
+						<div class="card--hidden">
+							<?php dynamic_sidebar('jan-picture-info'); ?>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row center col-sm-12 col-md-6 col-xl-3 animated fadeIn eds-on-scroll">
+				<div class="hover-images-effects">
+					<div class="card card-third">
+						<?php dynamic_sidebar('sarah-picture'); ?>
+						<div class="card--hidden">
+							<?php dynamic_sidebar('sarah-picture-info'); ?>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row center col-sm-12 col-md-6 col-xl-3 animated fadeIn eds-on-scroll">
+				<div class="hover-images-effects">
+					<div class="card card-third">
+						<?php dynamic_sidebar('christa-picture'); ?>
+						<div class="card--hidden">
+							<?php dynamic_sidebar('christa-picture-info'); ?>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</section>
+
+		<section class="row">
+
+			<div class="col-md-12">
+
+				<?php dynamic_sidebar('about-values-title'); ?>
+
+			</div>
+
+			<div class="row center col-sm-12 col-md-6">
+
+				<?php dynamic_sidebar('low-impact-value-icon'); ?>
+				<?php dynamic_sidebar('low-impact-value'); ?>
+
+			</div>
+
+			<div class="row center col-sm-12 col-md-6">
+
+				<?php dynamic_sidebar('recycling-programs-value-icon'); ?>
+				<?php dynamic_sidebar('recycling-programs-value'); ?>
+
+			</div>
+
+			<div class="row center col-sm-12 col-md-6">
+
+				<?php dynamic_sidebar('local-economy-value-icon'); ?>
+				<?php dynamic_sidebar('local-economy-value'); ?>
+
+			</div>
+
+			<div class="row center col-sm-12 col-md-6">
+
+				<?php dynamic_sidebar('those-in-need-value-icon'); ?>
+				<?php dynamic_sidebar('those-in-need-value'); ?>
+
+			</div>
+
+		</section>
+
+	</main>
 
 <?php get_footer(); ?>

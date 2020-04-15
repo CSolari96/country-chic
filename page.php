1fee07fd9c882db83b16307a56778dbaa7261914
Courtney Solari <csolari@ufl.edu>
2020-04-15 11:18:55 -0500

    Update content on default page template

--- page.php
+++ page.php
@@ -24,14 +24,10 @@
 
 	<div class="container">
 
-		<p>Testing</p>
-
 		<?php
 					if (have_posts()) {
 						while (have_posts()) {
 							the_post(); ?>
-
-							<h2><?php the_title(); ?></h2>
 
 							<?php the_content(); ?>
 				<?php

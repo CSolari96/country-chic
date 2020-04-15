749af848840d114704596e427631d37beeeb9262
Courtney Solari <csolari@ufl.edu>
2020-04-15 10:48:40 -0500

    Add banner to default page file

--- index.php
+++ index.php
@@ -2,6 +2,24 @@
 
 <main class="container-fluid">
 
+	<?php $background_img = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full'); ?>
+
+	<div class="jumbotron jumbotron-fluid" style="background-image: url(<?php echo $background_img; ?>)">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h1 class="hero-title"> <?php the_title(); ?> </h1>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<!--<div class="post-featured-image">
+		<?php the_post_thumbnail('large'); ?>
+	</div>-->
+
+
 	<div class="row">
 
 		<?php

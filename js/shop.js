63dba7099ce1eb3a086edb37183542204fea833a
Courtney Solari <csolari@ufl.edu>
2020-04-15 15:34:10 -0500

    Create JS to add back link to product pages

---
+++ js/shop.js
@@ -0,0 +1,25 @@
+// Create new link
+var newLink = document.createElement("a");
+var linkText = document.createTextNode("Go to the previous page");
+newLink.appendChild(linkText);
+
+// Set link attributes
+newLink.href = document.referrer;
+newLink.id = "back-link";
+
+// Locate and store product or collection container
+var collectionContainer = document.getElementsByClassName("wps-collection-single")[0];
+var productContainer = document.getElementsByClassName("wps-product-single")[0];
+
+// Create variable to hold page container
+var pageContainer;
+
+// Check if it is a single product or single collection page and set appropriate page container
+if (typeof(collectionContainer) != 'undefined' && collectionContainer != null) {
+	pageContainer = collectionContainer.parentElement;
+} elseif (typeof(productContainer) != 'undefined' && productContainer != null) {
+	pageContainer = productContainer.parentElement;
+}
+
+// Add link above product information
+pageContainer.insertBefore(newLink, productContainer);

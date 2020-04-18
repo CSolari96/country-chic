9aab146708ed94960500bd788671ea769db3d296
Valerie Bragg <vbragg@ufledu>
2020-04-18 17:46:16 -0400

    Edit lis tutorials 12

--- single-tutorial_types.php
+++ single-tutorial_types.php
@@ -8,14 +8,14 @@
 	<div class="row">
 		<aside class="col-md-3 products-list">
 			<div class="all-products">
-				<h3 class="products-used-title">Country Chic Products Used</h3>
+				<h3 class="products-used-title">Country Chic Products</h3>
 
 	<!---Makes each item under the products category in Advanced Custom Field on a single bullet-->
 				<?php
 				$field = get_field_object('product');
 				$fieldValues =  '<li>' . implode('<li>', $field['value']) . '</li>';
 				?>
-				<ul><?php echo '<li>' . $fieldValues . '</li>' ?> </ul>
+				<ul><?php echo $fieldValues ?> </ul>
 
 
 		<!---Makes each item under the products category in Advanced Custom Field on a single bullet-->

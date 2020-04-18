fc595bc26bf49ee5c366f77a82b9ac7c15bd2c57
Valerie Bragg <vbragg@ufledu>
2020-04-17 23:31:55 -0400

    Add readmore.js file

--- js/main.js
+++ js/main.js
@@ -18,31 +18,3 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		menuOpen.classList.remove("hide");
 	});
 });
-
-//READ MORE BUTTON ON ABOUT PAGE
-
-//VARIABLES
-
-var readButton = document.getElementById("buttonshow");
-var showLess = document.getElementById("buttonhide");
-var moreInfo = document.getElementById("moreinfo-about");
-
-//FUNCTION
-
-function revealMore () {
-    moreInfo.classList.remove('hide');
-    showLess.classList.remove('hide');
-    readButton.classList.add('hide');
-};
-
-function hideMore () {
-  moreInfo.classList.add('hide');
-  showLess.classList.add('hide');
-  readButton.classList.remove('hide');
-};
-
-//EVENT LISTENERS
-
-
-readButton.addEventListener('click', revealMore);
-showLess.addEventListener('click', hideMore);

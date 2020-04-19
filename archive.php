ce518cdec4c0667e31bdfcd9474d71b5943ee010
Courtney Solari <csolari@ufl.edu>
2020-04-18 23:20:12 -0500

    Update archives page

--- archive.php
+++ archive.php
@@ -1,48 +1,94 @@
+
 <?php get_header(); ?>
 
-	<div class="container">
+	<?php 
+
+		$posts_page_id = get_option( 'page_for_posts' );
+		$background_img = wp_get_attachment_image_src( get_post_thumbnail_id($posts_page_id), 'full'); 
+
+	?>
+
+	<main class="container blog">
 
 		<div class="row">
 
-			<?php
-				if (have_posts()) {
-					while (have_posts()) {
-						the_post(); ?> 
+			<div class="col-md-12">
 
-						<div class="col-md-12 post-preview">
-
-							 <h3><?php the_title(); ?></h3>
-
-							<p class="post-info">
-
-								<?php 
-									echo "Written by: " . get_the_author(); 
-									echo " <br /> "; 
-									publish_date();
-								?>
-									
-							</p>
-
-							<p class="category">Category: <?php the_category(' , '); ?></p>
-
-							<div class="post-featured-image">
-
-								<?php the_post_thumbnail("medium"); ?>
-
-							</div>
-
-							<?php the_excerpt(); ?>
-
-							<a class="btn btn-sm" href="<?php the_permalink(); ?>">Read More</a>  
-
-						</div>
-			<?php	}
-				}
+				<?php 
+					
+					if (is_day()) {
+						echo "<h2>Daily Archives: " . get_the_date() . "</h2>";
+					} elseif (is_month()) {
+						echo "<h2>Monthly Archives: " . get_the_date("F Y") . "</h2>";
+					} elseif (is_year()) {
+						echo "<h2>Yearly Archives: " . get_the_date("Y") . "</h2>";
+					} 
 
 				?>
 
+			</div>
+
 		</div>
 
-	</div>
+		<div class="row">
+
+			<div class="col-sm-12 col-md-8">
+
+				<?php
+					if (have_posts()) {
+						while (have_posts()) {
+							the_post(); ?>
+
+							<section class="post-preview">
+
+								<div class="post-featured-image">
+
+									<?php the_post_thumbnail("small"); ?>
+
+								</div>
+
+								<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+								<?php the_excerpt(); ?>
+
+								<a href="<?php the_permalink(); ?>">Read More</a>
+
+							</section>
+				<?php
+						}  // End while ?>
+
+						<div class="pagination-links">
+
+						<?php
+					       global $wp_query;
+
+					       $big = 999999999; // need an unlikely integer
+
+					       echo paginate_links( array(
+					       'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+					       'format' => '?paged=%#%',
+					       'current' => max( 1, get_query_var('paged') ),
+					       'total' => $wp_query->max_num_pages
+					        ) );
+
+						?>
+
+					</div>
+				<?php	}  // End if
+
+				?>
+
+			</div>
+
+			<aside class="col-sm-12 col-md-3">
+
+				<?php dynamic_sidebar('right-sidebar'); ?>
+
+			</aside>
+
+		</div>
+
+	</main>
+
 
 <?php get_footer(); ?>

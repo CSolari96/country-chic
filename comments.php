9c0c000e1e19c3f06dc23dafd5797517983509b7
Courtney Solari <csolari@ufl.edu>
2020-04-18 22:44:59 -0500

    Remove avatar from comments

--- comments.php
+++ comments.php
@@ -3,12 +3,8 @@
 	foreach ($comments as $comment) {
 
 		echo "<div class='comment'>";
-		echo "<div class='row'>";
-		echo "<div class='comment-avatar col-2'>";
-		echo get_avatar($comment, 32);
-		echo "</div>";
 
-		echo "<div class='comment-author col-10'>" . get_comment_author_link() . "</div></div>";
+		echo "<div class='comment-author'>" . get_comment_author_link() . "</div>";
 
 		echo "<div class='comment-date'>" . get_comment_date() . "</div>";
 
